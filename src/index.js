5247d66d52b4450be050e111cf638d582321e8db
brainrepo <murru7@gmail.com>
2022-10-03 22:57:17 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import InterfaceComponent from './interface.vue';
+
+export default {
+	id: 'custom',
+	name: 'Custom',
+	icon: 'face',
+	description: 'This is my custom interface!',
+	component: InterfaceComponent,
+	options: null,
+	types: ['string'],
+};
